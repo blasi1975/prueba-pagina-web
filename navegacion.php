9dadf90005ea8e5becd1dd59e995851de08b6c27
blasi1975 <68968732+blasi1975@users.noreply.github.com>
2020-07-29 20:17:13 +0100

    Add files via upload

---
+++ navegacion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="./estilos/navegacion.css">
+</head>
+
+<body>
+    <div id="grid">
+        <nav>
+            <ul>
+                <li><a href="quienesSomos.html">QUIENES SOMOS</a></li>
+                <li><a href="servicios.html">SERVICIOS</a></li>
+                <li><a href="noticias.html">NOTICIAS</a></li>
+                <li><a href="notasDePrensa.html">N. DE PRENSA</a></li>
+                <li><a href="formacion.html">FORMACIÓN</a></li>
+                <li><a href="mujerEmprendedora.html">EMPRENDEDORA</a></li>
+                <li><a href="financiacion.html">FINANCIACIÓN</a></li>
+                <li><a href="bolsaDeTrabajo.html">TRABAJO</a></li>
+                <li><a href="asociarse.html">ASOCIARSE</a></li>
+                <li><a href="contacto.html">CONTACTO</a></li>
+            </ul>
+
+        </nav>
+    </div>
+</body></html>
